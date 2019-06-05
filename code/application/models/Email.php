59c35263129ca52fdbb5c41b8f65c5eb346620ce
Dave Buchanan <dave@joesvolcano.net>
2019-06-05 11:00:18 -0600

    test pass without complaint now

--- code/application/models/Email.php
+++ code/application/models/Email.php
@@ -4,13 +4,13 @@ class Email
 {
     private $email;
 
-    private function __construct(string $email) {
+    private function __construct($email) {
         $this->ensureIsValidEmail($email);
 
         $this->email = $email;
     }
 
-    public static function fromString(string $email) {
+    public static function fromString($email) {
         return new self($email);
     }
 
@@ -18,7 +18,7 @@ class Email
         return $this->email;
     }
 
-    private function ensureIsValidEmail(string $email) {
+    private function ensureIsValidEmail($email) {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             throw new InvalidArgumentException(
                 sprintf(
